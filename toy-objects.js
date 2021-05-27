fcd56a9a8cc3a0edc9ec751508f65b75c4ec8d7f
sxiong0519 <sxiong0519@gmail.com>
2021-05-27 10:30:41 -0500

    creating three toys

--- toy-objects.js
+++ toy-objects.js
@@ -1,1 +1,21 @@
+const train = {
+    id: 1,
+    name: "train",
+    price: 5,
+    maker: "Fisher Price"    
+}
+
+const pony = {
+    id: 2,
+    name: "pony",
+    price: 7,
+    maker: "My Little Pony"
+}
+
+const doll = {
+    id: 3,
+    name: "barbie",
+    price: 14,
+    maker: "Mattel, Inc"
+}
 
